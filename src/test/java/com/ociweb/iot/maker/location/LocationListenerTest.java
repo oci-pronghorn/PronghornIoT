0340d2a704dea3d560ec183d974f875c50d6e85a
Tobias Schweiger <schweigert@objectcomputing.com>
2018-06-25 13:38:42 -0500

    boilerplate setup for location and calibration unit tests

--- src/test/java/com/ociweb/iot/maker/location/LocationListenerTest.java
+++ src/test/java/com/ociweb/iot/maker/location/LocationListenerTest.java
@@ -1,4 +1,33 @@
 package com.ociweb.iot.maker.location;
 
+import com.ociweb.gl.api.Behavior;
+import com.ociweb.iot.maker.FogApp;
+import com.ociweb.iot.maker.FogRuntime;
+import com.ociweb.iot.maker.Hardware;
+import com.ociweb.iot.maker.LocationListener;
+import org.junit.Test;
+
 public class LocationListenerTest {
+
+    @Test
+    public void correctLocationBehaviorTest() {
+
+        FogRuntime runtime = FogRuntime.run(new FogApp() {
+            @Override
+            public void declareConnections(Hardware builder) {
+
+            }
+
+            @Override
+            public void declareBehavior(FogRuntime runtime) {
+                runtime.registerListener(new LocationListener() {
+                    @Override
+                    public boolean location(int location, long oddsOfRightLocation, long totalSum) {
+                        return false;
+                    }
+                });
+            }
+        });
+    }
+
 }
--- src/test/java/com/ociweb/iot/maker/location/TrainingTest.java
+++ src/test/java/com/ociweb/iot/maker/location/TrainingTest.java
@@ -1,4 +1,29 @@
 package com.ociweb.iot.maker.location;
 
+import com.ociweb.iot.maker.*;
+import org.junit.Test;
+
 public class TrainingTest {
+    @Test
+    public void calibrationAndTrainingTest() {
+
+        FogRuntime runtime = FogRuntime.run(new FogApp() {
+
+            @Override
+            public void declareConnections(Hardware builder) {
+
+            }
+
+            @Override
+            public void declareBehavior(FogRuntime runtime) {
+                runtime.registerListener(new CalibrationListener() {
+                    @Override
+                    public boolean finishedCalibration(int startLocation, int units) {
+                        return false;
+                    }
+                });
+            }
+        });
+    }
+
 }
